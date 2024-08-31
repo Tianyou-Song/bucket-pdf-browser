132da40df62085389e37ec9461337c5c0e2321c7
Tianyou-Song <tim.d.song@gmail.com>
2024-08-31 12:56:39 -0700

    fix build

--- components/pdfViewer.tsx
+++ components/pdfViewer.tsx
@@ -8,10 +8,12 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 import type { PDFDocumentProxy } from 'pdfjs-dist';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-	'pdfjs-dist/build/pdf.worker.min.mjs',
-	import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+// 	'pdfjs-dist/build/pdf.worker.min.mjs',
+// 	import.meta.url,
+// ).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const options = {
 	cMapUrl: '/cmaps/',
