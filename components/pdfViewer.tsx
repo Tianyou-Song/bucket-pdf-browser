54960d684b348bc7013dcae46490de03b57f3133
Tianyou-Song <tim.d.song@gmail.com>
2024-08-31 13:45:39 -0700

    attempt fix

--- components/pdfViewer.tsx
+++ components/pdfViewer.tsx
@@ -25,15 +25,20 @@ const resizeObserverOptions = {};
 const maxWidth = 800;
 
 const PdfViewer = ({ fileUrl }: { fileUrl: string }) => {
-	const [numPages, setNumPages] = useState<number>();
+	const [containerHeight, setContainerHeight] = useState<number>();
 	const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
 	const [containerWidth, setContainerWidth] = useState<number>();
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [numPages, setNumPages] = useState<number>();
+	const [touchEndX, setTouchEndX] = useState(0);
+	const [touchStartX, setTouchStartX] = useState(0);
 
 	const onResize = useCallback<ResizeObserverCallback>((entries) => {
 		const [entry] = entries;
 
 		if (entry) {
 			setContainerWidth(entry.contentRect.width);
+			setContainerHeight(entry.contentRect.height);
 		}
 	}, []);
 
@@ -43,16 +48,59 @@ const PdfViewer = ({ fileUrl }: { fileUrl: string }) => {
 		setNumPages(nextNumPages);
 	};
 
+	const handleNextPage = () => {
+		if (currentPage < (numPages || 0)) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
+	const handlePreviousPage = () => {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
+	const handleTouchStart = (e: React.TouchEvent) => {
+		setTouchStartX(e.targetTouches[0].clientX);
+	};
+
+	const handleTouchMove = (e: React.TouchEvent) => {
+		setTouchEndX(e.targetTouches[0].clientX);
+	};
+
+	const handleTouchEnd = () => {
+		if (touchStartX - touchEndX > 50) {
+			handleNextPage();
+		}
+
+		if (touchStartX - touchEndX < -50) {
+			handlePreviousPage();
+		}
+	};
+
 	return (
 		<div className="min-h-screen w-screen" ref={setContainerRef}>
+			<div
+				className="absolute top-0 left-0 h-full w-1/2 cursor-pointer"
+				onClick={handlePreviousPage}
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				onTouchEnd={handleTouchEnd}
+				style={{ zIndex: 10 }}
+			/>
+			<div
+				className="absolute top-0 right-0 h-full w-1/2 cursor-pointer"
+				onClick={handleNextPage}
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				onTouchEnd={handleTouchEnd}
+				style={{ zIndex: 10 }}
+			/>
 			<Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-				{Array.from(new Array(numPages), (_el, index) => (
-					<Page
-						key={`page_${index + 1}`}
-						pageNumber={index + 1}
-						width={containerWidth}
-					/>
-				))}
+				<Page
+					pageNumber={currentPage}
+					width={containerWidth}
+				/>
 			</Document>
 		</div>
 	);
